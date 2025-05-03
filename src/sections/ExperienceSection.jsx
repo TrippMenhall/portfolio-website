b944953afe71fc59edb0098f8f74686255a94043
TrippMenhall <dalton.menhall@quinnipiac.edu>
2025-05-03 16:57:02 -0400

    Added Full Resume

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -24,7 +24,7 @@ export default function ExperienceSection() {
         languages={['3.68', 'Dean\'s List', 'Class of 2027']}
       />
       <a
-        href="/resume.pdf"
+        href="/portfolio-website/resume.pdf"
         className={styles.resumeLink}
         target="_blank"
         rel="noopener noreferrer"
